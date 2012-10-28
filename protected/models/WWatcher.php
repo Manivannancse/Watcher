d6fe90cc0b10fcf4ee0b06a7c89a026eeac539e0
JavaXu <xu.java@gmail.com>
2012-10-28 20:34:24 +0800

    规范代码缩进

--- protected/models/WWatcher.php
+++ protected/models/WWatcher.php
@@ -55,29 +55,30 @@ class WWatcher{
 	 * 比较传入的用户名和密码与数据库里值是否匹配
 	 */
 	public static function checkName($name,$password){
-		$watcher = new Watcher();
-		$criteria 	= new EMongoCriteria();
+		$watcher 		= new Watcher();
+		$criteria 		= new EMongoCriteria();
 		$criteria->name = $name;
-		$criteria->pwd = $password;
+		$criteria->pwd 	= $password;
 		$criteria->auth = 1;
-		$onePerson = $watcher->find($criteria);
+		$onePerson 		= $watcher->find($criteria);
 		if ($onePerson) {
 			$_SESSION['watcherID'] = $onePerson->_id;
 			return  true;
 		}
 		return false;
 	}
+	
 	public static function insertMongo($userName,$email,$password){
 		//不能为空
 		if(!$userName || !$email || !$password){
 			return false;
 		}
-		$watcher = new Watcher();
-		$watcher->name = $userName;
-		$watcher->email = $email;
-		$watcher->pwd = $password;
+		$watcher 				= new Watcher();
+		$watcher->name 			= $userName;
+		$watcher->email 		= $email;
+		$watcher->pwd 			= $password;
 		$watcher->lastLoginTime = time();
-		$watcher->auth = 1;
+		$watcher->auth 			= 1;
 		$watcher->insert();
 		if ($watcher->_id) {
 			return  true;
