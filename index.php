3903a08cd32a147d0173b296053df052293a6bd4
JavaXu <xu.java@gmail.com>
2012-10-30 15:36:59 +0800

    add index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$yii = dirname(__FILE__).'/../yii/framework/yii.php';
+$config = dirname(__FILE__).'/protected/config/main.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+
+require_once($yii);
+    require_once('phpChart/conf.php');
+Yii::createWebApplication($config)->run();
