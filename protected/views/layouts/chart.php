71e5ad182f41e190daf676af87d2901bcde6cfe1
JavaXu <xu.java@gmail.com>
2012-10-31 15:52:56 +0800

    test

--- protected/views/layouts/chart.php
+++ protected/views/layouts/chart.php
@@ -19,7 +19,6 @@
 		<div id="chart" style="position:absolute;top:40px;left:250px">
 			<?php echo $content;?>
 		</div>
-		
 		<div id="logoutDiv" style="position:absolute;top:40px;left:750px">
 			<a href="index.php?r=site/logout">Logout</a>
 		</div>
