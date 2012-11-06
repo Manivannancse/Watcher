1ccc41b8b2973da695d38253438a128dc8e91eeb
龚爱生 <gongaisheng@gmail.com>
2012-11-06 13:21:16 +0800

    修改页面

--- protected/views/watch/dangerTable.php
+++ protected/views/watch/dangerTable.php
@@ -1,20 +1,24 @@
 <html>
 <body>
-<h3>Record of more than 100W</h3>
-<table border="1" bgcolor="pink" width="1050" align="center">
+<h3>Record of more than 100W order by Total DESC ~~</h3>
+<table cellpadding="1" cellspacing="0"  border="1" bgcolor="#F3F3FA" width="1050" align="center">
 <tr>
 <?php 
+$num = 0;
 if($millionTable){
-	foreach($millionTable as $id => $tableArr){
 	?>
 		<tr>
-		<th>tableName</th>
-		<th>totalRecord</th>
-		<th>TlastUpdateTime</th>
-		<th>recordTime</th>
+		<th bgcolor="#d0d0d0"><font size="3" color="">tableName</font></th>
+		<th bgcolor="#d0d0d0"><font size="3" color="">totalRecord</font></th>
+		<th bgcolor="#d0d0d0"><font size="3" color="">TlastUpdateTime</font></th>
+		<th bgcolor="#d0d0d0"><font size="3" color="">recordTime</font></th>
 		</tr>
+	<?php 
+	foreach($millionTable as $id => $tableArr){
+		$num++;
+	?>
 		<tr>
-		<td><?=$tableArr['tableName'] ?></td>
+		<td><font color="#484891"><?=$tableArr['tableName'] ?></font></td>
 		<td><?=$tableArr['total'] ?></td>
 		<td><?=$tableArr['sectionTime'] ?></td>
 		<td><?=$tableArr['recordTime'] ?></td>
@@ -26,6 +30,7 @@ if($millionTable){
 }
 ?>
 </table>
+<h4><?=$num?> Records</h4>
 <p>&nbsp;</p>
 </body>
 </html>
