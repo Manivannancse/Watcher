a55e2b0b3810aa50099e9231c9b8c7b533306b6c
龚爱生 <gongaisheng@gmail.com>
2012-11-25 12:08:36 +0800

    查询超过100w的表记录

---
+++ protected/views/watch/dangerTable.php
@@ -0,0 +1,26 @@
+<h3>Record of more than 1000000</h3>
+<table border="1" bgcolor="pink" width="1050" align="center">
+<tr>
+<?php 
+if($millionTable){
+	foreach($millionTable as $tableArr){
+	?>
+		<tr>
+		<th>tableName</th>
+		<th>totalRecord</th>
+		<th>maxID</th>
+		<th>lastUpdateTime</th>
+		</tr>
+		<tr>
+		<td><?=$tableArr['tableName'] ?></td>
+		<td><?=$tableArr['total'] ?></td>
+		<td><?=$tableArr['maxID'] ?></td>
+		<td><?=$tableArr['sectionTime'] ?></td>
+		</tr>
+		<?php
+	}
+}
+?>
+
+
+
